7737176f044d7446d989d27644b802842da7c3ef
megbeckim@gmail.com <megbeckim@gmail.com@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2010-08-01 03:13:24 +0000

    added NetBeans project

---
+++ branch/OpenGLv2/Skylight1OpenGLDemo/src/com/sun/jsr239/wtksamples/cube/Cube.java
@@ -0,0 +1,108 @@
+/*
+ *
+ * Copyright (c) 2007, Sun Microsystems, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of Sun Microsystems nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.sun.jsr239.wtksamples.cube;
+
+import java.nio.*;
+
+import javax.microedition.khronos.egl.*;
+import javax.microedition.khronos.opengles.*;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.game.GameCanvas;
+import javax.microedition.midlet.MIDlet;
+
+
+
+
+public class Cube extends MIDlet implements CommandListener {
+    private final Command exitCommand = new Command("Exit", Command.EXIT, 1);
+    Display display;
+    CubeCanvas canvas;
+    boolean started = false;
+    boolean paused = false;
+    boolean finished = false;
+    Thread drawThread;
+
+    public Cube() {
+        this.display = Display.getDisplay(this);
+        this.canvas = new CubeCanvas(this);
+        this.canvas.setCommandListener(this);
+        this.canvas.addCommand(exitCommand);
+    }
+
+    public void startApp() {
+        if (!started) {
+            started = true;
+            display.setCurrent(canvas);
+            drawThread = new Thread(canvas);
+            drawThread.start();
+        }
+
+        paused = finished = false;
+    }
+
+    public void pauseApp() {
+        paused = true;
+    }
+
+    public void destroyApp(boolean unconditional) {
+        // Wait for draw thread to die
+        setFinished();
+
+        try {
+            drawThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paint(Graphics g) {
+    }
+
+    public void commandAction(Command cmd, Displayable disp) {
+        if (cmd == exitCommand) {
+            destroyApp(false);
+            notifyDestroyed();
+        }
+    }
+
+    public synchronized boolean isFinished() {
+        return finished;
+    }
+
+    public synchronized void setFinished() {
+        finished = true;
+    }
+}
