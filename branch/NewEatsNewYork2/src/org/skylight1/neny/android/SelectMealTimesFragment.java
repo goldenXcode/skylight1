08d4637fc43546a8328444a931b699782584ae64
dario.nycjava@gmail.com <dario.nycjava@gmail.com@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2013-06-07 05:26:56 +0000

    layout changes

--- branch/NewEatsNewYork2/src/org/skylight1/neny/android/SelectMealTimesFragment.java
+++ branch/NewEatsNewYork2/src/org/skylight1/neny/android/SelectMealTimesFragment.java
@@ -1,18 +1,20 @@
 package org.skylight1.neny.android;
 
+import org.skylight1.neny.android.database.dao.MealTimePreferences;
+import org.skylight1.neny.android.database.dao.PreferencesDao;
+import org.skylight1.neny.android.database.model.DayAndTime;
+import org.skylight1.neny.android.database.model.MealDayTime;
+
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
-import org.skylight1.neny.android.database.dao.MealTimePreferences;
-import org.skylight1.neny.android.database.dao.PreferencesDao;
-import org.skylight1.neny.android.database.model.DayAndTime;
-import org.skylight1.neny.android.database.model.MealDayTime;
 
 
 public class SelectMealTimesFragment extends Fragment {
@@ -65,6 +67,15 @@ public class SelectMealTimesFragment extends Fragment {
         }
 
         return times;
+    }
+    public void gotoDashboard(final View aView){
+  		final Intent showRestaurantsIntent = new Intent(getActivity(), ShowRestaurantListActivity.class);
+  		String wizardString = getResources().getString(R.string.wizard_complete);
+  		SharedPreferences preferences = getActivity().getSharedPreferences(wizardString, Context.MODE_PRIVATE);
+  		Editor edit = preferences.edit();
+  		edit.putBoolean(wizardString, true);
+  		edit.commit();
+  		startActivity(showRestaurantsIntent);
     }
 
 }
