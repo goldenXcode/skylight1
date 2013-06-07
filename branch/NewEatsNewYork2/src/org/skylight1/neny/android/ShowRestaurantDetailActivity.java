044658891a3572db1dc00879a79c0f3204998ded
parth.thirteen@gmail.com <parth.thirteen@gmail.com@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2013-06-07 03:55:20 +0000

    Fixed the restaurant detail view. Turned the text into links. And removed the call button. Now going to make the image larger

--- branch/NewEatsNewYork2/src/org/skylight1/neny/android/ShowRestaurantDetailActivity.java
+++ branch/NewEatsNewYork2/src/org/skylight1/neny/android/ShowRestaurantDetailActivity.java
@@ -7,20 +7,25 @@ import org.skylight1.neny.android.database.model.Restaurant;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ShowRestaurantDetailActivity extends Activity implements View.OnTouchListener{
+public class ShowRestaurantDetailActivity extends Activity implements
+		OnClickListener {
 
 	public final static String CITY = "New York,NY";
 	private Restaurant selectedRestaurant = null;
-	private final String TAG = ShowRestaurantDetailActivity.class.getSimpleName();
+	private final String TAG = ShowRestaurantDetailActivity.class
+			.getSimpleName();
+
 	@Override
 	protected void onCreate(Bundle aSavedInstanceState) {
 		super.onCreate(aSavedInstanceState);
@@ -31,7 +36,8 @@ public class ShowRestaurantDetailActivity extends Activity implements View.OnTou
 		if (extras != null) {
 			String camis = extras.getString("camis");
 			if (camis != null) {
-				final Restaurant restaurant = new RestaurantDatabase(this).getRestaurantByCamis(camis);
+				final Restaurant restaurant = new RestaurantDatabase(this)
+						.getRestaurantByCamis(camis);
 				if (restaurant != null) {
 					selectedRestaurant = restaurant;
 					showRestaurantDetail(restaurant);
@@ -46,45 +52,44 @@ public class ShowRestaurantDetailActivity extends Activity implements View.OnTou
 
 		final TextView tvRestaurantPhone = (TextView) findViewById(R.id.tv_detail_restaurant_phone);
 		tvRestaurantPhone.setText(restaurant.getPhone());
-
-		final Button callButton = (Button) findViewById(R.id.bn_call);
-		callButton.setTag(restaurant.getPhone());
+		tvRestaurantPhone.setOnClickListener(this);
+		tvRestaurantPhone.setTag(restaurant.getPhone());
+		tvRestaurantPhone.setPaintFlags(tvRestaurantPhone.getPaintFlags()
+				| Paint.UNDERLINE_TEXT_FLAG);
 
 		final Address address = restaurant.getAddress();
 		final TextView tvStreet = (TextView) findViewById(R.id.tv_detail_restaurant_street);
-		final TextView tvZipCode = (TextView) findViewById(R.id.tv_detail_restaurant_zipcode);
 		final String street = address.getBuilding() + " " + address.getStreet();
-		tvStreet.setText(street);
-		tvStreet.setOnTouchListener(this);
-		tvZipCode.setText(address.getZipCode());
+		tvStreet.setText(street + "\n" + address.getZipCode());
+		tvStreet.setPaintFlags(tvStreet.getPaintFlags()
+				| Paint.UNDERLINE_TEXT_FLAG);
+		tvStreet.setOnClickListener(this);
 		final ImageView cuisineImageView = (ImageView) findViewById(R.id.cuisine_image);
-		final Cuisine cuisine = Cuisine.findCuisineByMajorCuisineLabel(restaurant.getMajorCuisine());
+		final Cuisine cuisine = Cuisine
+				.findCuisineByMajorCuisineLabel(restaurant.getMajorCuisine());
 		cuisineImageView.setImageResource(cuisine.getActiveImageResourceId());
 		final TextView cuisineNameView = (TextView) findViewById(R.id.cuisine_image_text);
 		cuisineNameView.setText(cuisine.getLabel());
 	}
 
-	public void callRestaurant(View v) {
-		final Intent callIntent = new Intent(Intent.ACTION_CALL);
-		callIntent.setData(Uri.parse("tel:" + v.getTag()));
-		startActivity(callIntent);
-	}
-	
-
-
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		if(selectedRestaurant !=null){
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.tv_detail_restaurant_street:
 			Address addRess = selectedRestaurant.getAddress();
-			final String queryString = addRess.getBuilding()+" "+addRess.getStreet()+" "+CITY+" "+addRess.getZipCode();
+			final String queryString = addRess.getBuilding() + " "
+					+ addRess.getStreet() + " " + CITY + " "
+					+ addRess.getZipCode();
 			Log.d(TAG, queryString);
-			Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
-				    Uri.parse("http://maps.google.com/maps?q="+queryString));
+			Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+					Uri.parse("http://maps.google.com/maps?q=" + queryString));
 			startActivity(intent);
-			return true;
+			break;
+		case R.id.tv_detail_restaurant_phone:
+			final Intent callIntent = new Intent(Intent.ACTION_CALL);
+			callIntent.setData(Uri.parse("tel:" + v.getTag()));
+			startActivity(callIntent);
+			break;
 		}
-		
-			
-		return false;
 	}
 }
