37908332ba3a3ecf75977acef9097a65a27af0cf
parth.thirteen@gmail.com <parth.thirteen@gmail.com@9ff7648e-257f-11de-9b37-5dcb32d212a7>
2013-06-07 04:43:58 +0000

    Removed the headers, and the buttons on the bottom

--- branch/NewEatsNewYork2/src/org/skylight1/neny/android/SelectCuisinesFragment.java
+++ branch/NewEatsNewYork2/src/org/skylight1/neny/android/SelectCuisinesFragment.java
@@ -20,120 +20,137 @@ import static java.util.Arrays.asList;
 
 public class SelectCuisinesFragment extends Fragment {
 
-    public static final List<Integer> CUISINE_ACTIVE_IMAGES = asList(R.drawable.china_active, R.drawable.africa_active, R.drawable.italian_active, R.drawable.mayan_active, R.drawable.comfort_active, R.drawable.eclectic_active, R.drawable.eu_active, R.drawable.indian_active, R.drawable.middle_eastern_active, R.drawable.north_america_active, R.drawable.pacifica_active, R.drawable.vege_active);
+	public static final List<Integer> CUISINE_ACTIVE_IMAGES = asList(
+			R.drawable.china_active, R.drawable.africa_active,
+			R.drawable.italian_active, R.drawable.mayan_active,
+			R.drawable.comfort_active, R.drawable.eclectic_active,
+			R.drawable.eu_active, R.drawable.indian_active,
+			R.drawable.middle_eastern_active, R.drawable.north_america_active,
+			R.drawable.pacifica_active, R.drawable.vege_active);
 
-    private List<Boolean> listOfSelectedCuisines = new ArrayList<Boolean>();
-    private PreferencesDao preferencesDao;
-    GridView grid;
+	private List<Boolean> listOfSelectedCuisines = new ArrayList<Boolean>();
+	private PreferencesDao preferencesDao;
+	GridView grid;
 
-    public static SelectCuisinesFragment newInstance() {
-        SelectCuisinesFragment fragment = new SelectCuisinesFragment();
+	public static SelectCuisinesFragment newInstance() {
+		SelectCuisinesFragment fragment = new SelectCuisinesFragment();
 
-        return fragment;
-    }
+		return fragment;
+	}
 
-    @Override
-    public void onCreate(Bundle aSavedInstanceState) {
+	@Override
+	public void onCreate(Bundle aSavedInstanceState) {
 
-        super.onCreate(aSavedInstanceState);
+		super.onCreate(aSavedInstanceState);
 
-        preferencesDao = new CuisinePreferences(getActivity());
+		preferencesDao = new CuisinePreferences(getActivity());
 
-    }
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 
-        View mView;
+		View mView;
 
-        mView = inflater.inflate(R.layout.cuisines_view, container, false);
+		mView = inflater.inflate(R.layout.cuisines_view, container, false);
 
-        final GridView grid = (GridView) mView.findViewById(R.id.cuisinesGrid);
-        final List<Integer> cuisinesActiveImageResources =
-                CUISINE_ACTIVE_IMAGES;
-        final List<Integer> cuisinesInactiveImageResources =
-                asList(R.drawable.china_inactive, R.drawable.africa_inactive, R.drawable.italian_inactive, R.drawable.mayan_inactive, R.drawable.comfort_inactive, R.drawable.eclectic_inactive, R.drawable.eu_inactive, R.drawable.indian_inactive, R.drawable.middle_eastern_inactive, R.drawable.north_america_inactive, R.drawable.pacifica_inactive, R.drawable.vege_inactive);
-        for (final int dummy : cuisinesActiveImageResources) {
-            listOfSelectedCuisines.add(false);
-        }
+		final GridView grid = (GridView) mView.findViewById(R.id.cuisinesGrid);
+		final List<Integer> cuisinesActiveImageResources = CUISINE_ACTIVE_IMAGES;
+		final List<Integer> cuisinesInactiveImageResources = asList(
+				R.drawable.china_inactive, R.drawable.africa_inactive,
+				R.drawable.italian_inactive, R.drawable.mayan_inactive,
+				R.drawable.comfort_inactive, R.drawable.eclectic_inactive,
+				R.drawable.eu_inactive, R.drawable.indian_inactive,
+				R.drawable.middle_eastern_inactive,
+				R.drawable.north_america_inactive,
+				R.drawable.pacifica_inactive, R.drawable.vege_inactive);
+		for (final int dummy : cuisinesActiveImageResources) {
+			listOfSelectedCuisines.add(false);
+		}
 
-        // load preferences
-        listOfSelectedCuisines = new ArrayList<Boolean>();
-        for (int i = 0; i < cuisinesActiveImageResources.size(); i++) {
-            listOfSelectedCuisines.add(preferencesDao.getPreference(mapImagePositionsToEnums(i).getLabel(), true));
-        }
+		// load preferences
+		listOfSelectedCuisines = new ArrayList<Boolean>();
+		for (int i = 0; i < cuisinesActiveImageResources.size(); i++) {
+			listOfSelectedCuisines.add(preferencesDao.getPreference(
+					mapImagePositionsToEnums(i).getLabel(), true));
+		}
 
-        grid.setAdapter(new CuisineAdapter(this.getActivity(), cuisinesActiveImageResources, listOfSelectedCuisines, cuisinesInactiveImageResources));
+		grid.setAdapter(new CuisineAdapter(this.getActivity(),
+				cuisinesActiveImageResources, listOfSelectedCuisines,
+				cuisinesInactiveImageResources));
 
-        return mView;
-    }
+		return mView;
+	}
 
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
+	@Override
+	public void setUserVisibleHint(boolean isVisibleToUser) {
+		super.setUserVisibleHint(isVisibleToUser);
 
-        if(isVisibleToUser) {
-            SharedPreferences preferences = getActivity().getSharedPreferences(PreferencesActivity.WizardState, Context.MODE_PRIVATE);
-            if(!preferences.getBoolean(PreferencesActivity.CuisineState, false)) {
-                SharedPreferences.Editor edit = preferences.edit();
-                edit.putBoolean(PreferencesActivity.CuisineState, true);
-                edit.commit();
-            }
-        }
-    }
+		if (isVisibleToUser) {
+			SharedPreferences preferences = getActivity().getSharedPreferences(
+					PreferencesActivity.WizardState, Context.MODE_PRIVATE);
+			if (!preferences
+					.getBoolean(PreferencesActivity.CuisineState, false)) {
+				SharedPreferences.Editor edit = preferences.edit();
+				edit.putBoolean(PreferencesActivity.CuisineState, true);
+				edit.commit();
+			}
+		}
+	}
 
-    public static Cuisine mapImagePositionsToEnums(int position) {
-        Cuisine cuisine = null;
-        switch (position) {
-            case 0: {
-                cuisine = Cuisine.ASIAN;
-                break;
-            }
-            case 1: {
-                cuisine = Cuisine.AFRICAN;
-                break;
-            }
-            case 2: {
-                cuisine = Cuisine.ITALIAN;
-                break;
-            }
-            case 3: {
-                cuisine = Cuisine.CENTRAL_AND_SOUTH_AMERICAN;
-                break;
-            }
-            case 4: {
-                cuisine = Cuisine.COMFORT_AND_SNACKS;
-                break;
-            }
-            case 5: {
-                cuisine = Cuisine.ECLECTIC;
-                break;
-            }
-            case 6: {
-                cuisine = Cuisine.EUROPEAN;
-                break;
-            }
-            case 7: {
-                cuisine = Cuisine.INDIAN_SUBCONTINENT;
-                break;
-            }
-            case 8: {
-                cuisine = Cuisine.MIDDLE_EASTERN;
-                break;
-            }
-            case 9: {
-                cuisine = Cuisine.US_NORTH_AMERICAN;
-                break;
-            }
-            case 10: {
-                cuisine = Cuisine.PACIFICA;
-                break;
-            }
-            case 11: {
-                cuisine = Cuisine.VEGETARIAN;
-                break;
-            }
-        }
-        return cuisine;
-    }
+	public static Cuisine mapImagePositionsToEnums(int position) {
+		Cuisine cuisine = null;
+		switch (position) {
+		case 0: {
+			cuisine = Cuisine.ASIAN;
+			break;
+		}
+		case 1: {
+			cuisine = Cuisine.AFRICAN;
+			break;
+		}
+		case 2: {
+			cuisine = Cuisine.ITALIAN;
+			break;
+		}
+		case 3: {
+			cuisine = Cuisine.CENTRAL_AND_SOUTH_AMERICAN;
+			break;
+		}
+		case 4: {
+			cuisine = Cuisine.COMFORT_AND_SNACKS;
+			break;
+		}
+		case 5: {
+			cuisine = Cuisine.ECLECTIC;
+			break;
+		}
+		case 6: {
+			cuisine = Cuisine.EUROPEAN;
+			break;
+		}
+		case 7: {
+			cuisine = Cuisine.INDIAN_SUBCONTINENT;
+			break;
+		}
+		case 8: {
+			cuisine = Cuisine.MIDDLE_EASTERN;
+			break;
+		}
+		case 9: {
+			cuisine = Cuisine.US_NORTH_AMERICAN;
+			break;
+		}
+		case 10: {
+			cuisine = Cuisine.PACIFICA;
+			break;
+		}
+		case 11: {
+			cuisine = Cuisine.VEGETARIAN;
+			break;
+		}
+		}
+		return cuisine;
+	}
 }
